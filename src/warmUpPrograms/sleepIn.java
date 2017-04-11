1eb0c5f8b1b8bca924a64df1e631dc6bafa4d70e
gaurBrijesh <nanosoft10@gmail.com>
2017-04-12 00:18:31 +0530

    Sleep In Program

--- src/warmUpPrograms/sleepIn.java
+++ src/warmUpPrograms/sleepIn.java
@@ -3,6 +3,13 @@ package warmUpPrograms;
 /**
  * Created by brijesh on 12/4/17.
  */
+
+/*The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+
+        sleepIn(false, false) → true
+        sleepIn(true, false) → false
+        sleepIn(false, true) → true
+*/
 public class sleepIn {
     public boolean SleepIn(boolean weekdays, boolean vacation){
         if (!weekdays || vacation){
