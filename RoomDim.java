244f24a4545c2682413bdfc485202d8f38b11f10
gaurBrijesh <nanosoft10@gmail.com>
2017-04-21 00:56:00 +0530

    Room Dimension

--- RoomDim.java
+++ RoomDim.java
@@ -4,4 +4,16 @@ package warmUpPrograms;
  * Created by brijesh on 21/4/17.
  */
 public class RoomDim {
+    //RoomDim(int length, int width){
+      //  int area  = length * width;
+        //System.out.println("The room area is :" + area + " sqrmtr");
+    //}
+
+    public static void main(String[] args) {
+  //      new RoomDim(140,300);
+int length = 140;
+int width = 300;
+int area = length * width;
+        System.out.println("The room area is :" + area +  " sqrmtr");
+    }
 }
